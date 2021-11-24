9df65f52991dae62c60fd9da712944a4477df4eb
Jvieira03 <88110711+Jvieira03@users.noreply.github.com>
2021-11-24 20:15:34 -0300

    Update Ingresso.cpp

--- Ingresso.cpp
+++ Ingresso.cpp
@@ -1,3 +1,21 @@
+//Escreva um programa em C++ que imprime o preço de um ingresso para um parque de diversões. O programa deve ler, a partir do teclado, um número real representando o valor base do ingresso e um número inteiro representando a idade do usuário.
+
+//O valor do ingresso deve ser calculado de acordo com as seguintes condições:
+
+//Menores de 5 anos não pagam;
+//Crianças entre 5 e 12 anos pagam meia-entrada;
+//Adolescentes entre 13 e 17 anos têm 20% de desconto;
+//Adultos entre 18 e 70 anos pagam o valor-base;
+//Maiores de 70 anos não pagam;
+//OBS.: Nos exemplos, o texto em negrito/vermelho representa a entrada do usuário e o texto em itálico representa a saída do programa. 
+          // O programa deve se comportar como indicado nos exemplos abaixo, obtendo as entradas e mostrando as mensagens e resultados
+       // NA MESMA SEQUÊNCIA E FORMATO (incluindo os textos das mensagens).
+
+//Exemplo 1:
+//Digite o valor-base do ingresso: 50
+//Digite a idade do usuario: 15
+//Valor final do ingresso: 40
+
 // Programa sobre o valor do ingresso de cada idade
 #include <iostream>
 
